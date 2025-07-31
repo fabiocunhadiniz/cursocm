745819675cbd379c3d85dddf1d1be375c32d472a
fabiocunhadiniz <fabinhobjp@gmail.com>
2025-07-31 20:31:03 -0300

    Projeto foi Criado

---
+++ src/main/java/com/fabiodc/cursocm/CursocmApplication.java
@@ -0,0 +1,15 @@
+package com.fabiodc.cursocm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursocmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursocmApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/fabiodc/cursocm/CursocmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiodc.cursocm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursocmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
